98b56f83fdc3b80008bfb7b8d607a41d86c0177d
ao.chen@ucghub.net <ao.chen@ucghub.net>
2024-11-09 13:58:39 +0000

    test

---
+++ models/example/orderdetails.sql
@@ -0,0 +1,17 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+
+
+select *
+from stagging.order_details where created_at >'2024-10-01'
+
+
